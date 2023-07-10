e817d4eddc4e39b4bb4dabf9eeb6e10401ee916c
Eden Federman <eden@keyval.dev>
2023-07-10 16:52:06 +0300

    upgrade emailservice and recommendation service

--- src/emailservice/email_server.py
+++ src/emailservice/email_server.py
@@ -20,6 +20,7 @@ import os
 import sys
 import time
 import grpc
+import traceback
 from jinja2 import Environment, FileSystemLoader, select_autoescape, TemplateError
 from google.api_core.exceptions import GoogleAPICallError
 from google.auth.exceptions import DefaultCredentialsError
@@ -39,98 +40,93 @@ env = Environment(
 )
 template = env.get_template('confirmation.html')
 
-
 class BaseEmailService(demo_pb2_grpc.EmailServiceServicer):
-    def Check(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.SERVING)
-
+  def Check(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
+  
+  def Watch(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.UNIMPLEMENTED)
 
 class EmailService(BaseEmailService):
-    def __init__(self):
-        raise Exception('cloud mail client not implemented')
-        super().__init__()
+  def __init__(self):
+    raise Exception('cloud mail client not implemented')
+    super().__init__()
 
-    @staticmethod
-    def send_email(client, email_address, content):
-        response = client.send_message(
-            sender=client.sender_path(project_id, region, sender_id),
-            envelope_from_authority='',
-            header_from_authority='',
-            envelope_from_address=from_address,
-            simple_message={
-                "from": {
-                    "address_spec": from_address,
-                },
-                "to": [{
-                    "address_spec": email_address
-                }],
-                "subject": "Your Confirmation Email",
-                "html_body": content
-            }
-        )
-        logger.info("Message sent: {}".format(response.rfc822_message_id))
+  @staticmethod
+  def send_email(client, email_address, content):
+    response = client.send_message(
+      sender = client.sender_path(project_id, region, sender_id),
+      envelope_from_authority = '',
+      header_from_authority = '',
+      envelope_from_address = from_address,
+      simple_message = {
+        "from": {
+          "address_spec": from_address,
+        },
+        "to": [{
+          "address_spec": email_address
+        }],
+        "subject": "Your Confirmation Email",
+        "html_body": content
+      }
+    )
+    logger.info("Message sent: {}".format(response.rfc822_message_id))
 
-    def SendOrderConfirmation(self, request, context):
-        email = request.email
-        order = request.order
+  def SendOrderConfirmation(self, request, context):
+    email = request.email
+    order = request.order
 
-        try:
-            confirmation = template.render(order=order)
-        except TemplateError as err:
-            context.set_details(
-                "An error occurred when preparing the confirmation mail.")
-            logger.error(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
+    try:
+      confirmation = template.render(order = order)
+    except TemplateError as err:
+      context.set_details("An error occurred when preparing the confirmation mail.")
+      logger.error(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return demo_pb2.Empty()
 
-        try:
-            EmailService.send_email(self.client, email, confirmation)
-        except GoogleAPICallError as err:
-            context.set_details("An error occurred when sending the email.")
-            print(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
+    try:
+      EmailService.send_email(self.client, email, confirmation)
+    except GoogleAPICallError as err:
+      context.set_details("An error occurred when sending the email.")
+      print(err.message)
+      context.set_code(grpc.StatusCode.INTERNAL)
+      return demo_pb2.Empty()
 
-        return demo_pb2.Empty()
-
+    return demo_pb2.Empty()
 
 class DummyEmailService(BaseEmailService):
-    def SendOrderConfirmation(self, request, context):
-        logger.info('A request to send order confirmation email to {} has been received.'.format(
-            request.email))
-        return demo_pb2.Empty()
-
+  def SendOrderConfirmation(self, request, context):
+    logger.info('A request to send order confirmation email to {} has been received.'.format(request.email))
+    return demo_pb2.Empty()
 
 class HealthCheck():
-    def Check(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.SERVING)
-
+  def Check(self, request, context):
+    return health_pb2.HealthCheckResponse(
+      status=health_pb2.HealthCheckResponse.SERVING)
 
 def start(dummy_mode):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    service = None
-    if dummy_mode:
-        service = DummyEmailService()
-    else:
-        raise Exception('non-dummy mode not implemented yet')
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),)
+  service = None
+  if dummy_mode:
+    service = DummyEmailService()
+  else:
+    raise Exception('non-dummy mode not implemented yet')
 
-    demo_pb2_grpc.add_EmailServiceServicer_to_server(service, server)
-    health_pb2_grpc.add_HealthServicer_to_server(service, server)
+  demo_pb2_grpc.add_EmailServiceServicer_to_server(service, server)
+  health_pb2_grpc.add_HealthServicer_to_server(service, server)
 
-    port = os.environ.get('PORT', "8080")
-    logger.info("listening on port: "+port)
-    server.add_insecure_port('[::]:'+port)
-    server.start()
-    try:
-        while True:
-            time.sleep(3600)
-    except KeyboardInterrupt:
-        server.stop(0)
-
+  port = os.environ.get('PORT', "8080")
+  logger.info("listening on port: "+port)
+  server.add_insecure_port('[::]:'+port)
+  server.start()
+  try:
+    while True:
+      time.sleep(3600)
+  except KeyboardInterrupt:
+    server.stop(0)
 
 if __name__ == '__main__':
-    logger.info('starting the email service in dummy mode.')
-
-    start(dummy_mode=True)
+  logger.info('starting the email service in dummy mode.')
+  start(dummy_mode = True)
